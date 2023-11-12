c08a22dcaf0f0cb545b56985cfebb4a701ec7d28
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2023-11-12 22:28:52 +0700

    Your commit message here

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -4,7 +4,7 @@ import NavBar from '../components/navigation_bar' // ../ means parent directory,
 export default function Courses() {
   return (
     <>
-      <NavBar link1="/landing_page/" link2="/courses/" link3="/payment/"></NavBar>
+      <NavBar link1="/" link2="/courses/" link3="/payment/"></NavBar>
       <h1>Hello</h1>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import NavBar from './components/navigation_bar'
 export default function Home() {
   return (
     <>
-      <NavBar link1="/landing_page/" link2="/courses/" link3="/payment/"></NavBar>
+      <NavBar link1="/" link2="/courses/" link3="/payment/"></NavBar>
       <h1>Hello</h1>
     </>
   );
