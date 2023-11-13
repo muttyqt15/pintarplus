07f2d15ef1cacd998155bc9f25d54f57dcb8c9d2
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2023-11-13 20:29:28 +0700

    added colors and layout for coloring

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,46 +1,139 @@
 import Image from 'next/image';
 import NavBar from '../components/navigation_bar'; // ../ means parent directory, ./ means current directory
 import pythonImage from '../../../public/python_image.jpeg';
+import Layout from '../components/layout';
 
 
 export default function Courses() {
   return (
-    <>
-      <NavBar link1="/" link2="/courses/" link3="/payment/"></NavBar>
-      <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center backdrop-blur-md"></div>
-      <h1 className="flex flex-col items-center text-6xl font-bold mt-8">Courses</h1>
-      <div className="grid grid-cols-4 w-100 h-100 grid-rows-2 gap-4 mx-16 mt-8 place-items-center">
-        <button className="border border-slate-300 rounded-3xl shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
-          <div className="flex flex-col items-center">
-            <Image 
-            src={pythonImage}
-            alt="python"
-            className="course-img"
-            />
-            <h2>
-              Python 101
-            </h2>
-            <p className="p-2 text-xs">
-            Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
-            </p>
+    <Layout>
+      <h1 className="text-6xl font-bold flex justify-center mt-16">Courses</h1>
+      <div className="flex flex-col items-center" style={{ zIndex: 4 }}>
+          <div className="grid course-grid grid-cols-4 w-100 h-100 grid-rows-2 gap-4 mx-16 mt-20 place-items-center">
+            <button className="border border-slate-300 bg-slate-50 rounded-3xl shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+
+            
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
+            <button className="border border-slate-300 rounded-3xl bg-slate-50  shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+              <div className="flex flex-col items-center">
+                <Image 
+                src={pythonImage}
+                alt="python"
+                className="course-img"
+                />
+                <h2>
+                  Python 101
+                </h2>
+                <p className="p-2 text-xs">
+                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                </p>
+              </div>
+            </button>
           </div>
-        </button>
-        <button className="border border-slate-300 rounded-3xl shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
-          <div className="flex flex-col items-center">
-            <Image 
-            src={pythonImage}
-            alt="python"
-            className="course-img"
-            />
-            <h2>
-              Python 101
-            </h2>
-            <p className="p-2 text-xs">
-            Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
-            </p>
-          </div>
-        </button>
-      </div>
-    </>
+        </div>
+    </Layout>
   );
 }
