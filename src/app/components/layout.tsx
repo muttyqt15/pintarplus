399a2c33b12881845575800ef77d8ee32056a4e6
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2023-11-14 10:55:27 +0700

    added images and changed courses layout

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -9,11 +9,12 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
   return (
     <>
       <NavBar link1="/" link2="/courses/" link3="/payment/" />
-      <div className="min-h-screen flex items-center justify-center px-16 relative">
+      <div className="absolute -bottom-40 -left-28 w-96 h-96 bg-blue-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob"></div>
+      <div className="min-h-screen flex items-center justify-center px-8 relative">
         <div className="absolute">
-          <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-          <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-          <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+      
+          <div className="absolute -top-20 -left-36 w-96 h-96 rounded-full before bg-orange-300 filter blur-3xl mix-blend-multiply opacity-70 animate-blob animation-delay-2000"></div>
+          <div className="absolute -bottom-8 left-96 w-96 h-96 rounded-full before bg-pink-300 filter blur-3xl mix-blend-multiply opacity-70 animate-blob animation-delay-2000"></div>
         </div>
         <div className="relative z-10">{children}</div>
       </div>
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,134 +1,143 @@
 import Image from 'next/image';
 import NavBar from '../components/navigation_bar'; // ../ means parent directory, ./ means current directory
 import pythonImage from '../../../public/python_image.jpeg';
+import discreteMath from '../../../public/Rectangle 31.png';
+import bizAnalytics from '../../../public/bizAnalytics.jpeg';
+import calculusImg from '../../../public/calculus_img.jpg';
+import dataScienceImg from '../../../public/datascienceImg.jpeg';
+import linearAlgebraImg from '../../../public/linearAlgebra.jpeg';
+import productManagementImg from '../../../public/productManagementImg.jpeg';
+
+
 import Layout from '../components/layout';
 
 
 export default function Courses() {
   return (
     <Layout>
-      <h1 className="text-6xl font-bold flex justify-center mt-16">Courses</h1>
+      <div className="absolute top-36 -right-full w-96 h-96 bg-purple-950 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+      <h1 className="text-6xl font-bold flex justify-center mt-8">Courses</h1>
       <div className="flex flex-col items-center" style={{ zIndex: 4 }}>
-          <div className="grid course-grid grid-cols-4 w-100 h-100 grid-rows-2 gap-4 mx-16 mt-20 place-items-center">
-            <button className="border border-slate-300 bg-slate-50 rounded-3xl shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+          <div className="grid course-grid grid-cols-4 w-100 h-100 grid-rows-2 gap-2 mx-16 mt-10 place-items-center">
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
                 src={pythonImage}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Python   101
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
-                alt="python"
+                src={discreteMath}
+                alt="discrete math"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Discrete Math
                 </h2>
-                <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                <p className="p-2 text-xs font-">
+                Discrete math is a math to learn about the various wonders of the world! 
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
+                src={bizAnalytics}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Business Analytics
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Business analytics is a great career path with multiple online courses.
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
+                src={calculusImg}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Calculus 2
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
+                src={dataScienceImg}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Data Science
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
 
             
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
+                src={linearAlgebraImg}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Linear Algebra
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50 shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
-                src={pythonImage}
+                src={productManagementImg}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
-                  Python 101
+                <h2 className="font-semibold">
+                  Project Management
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
-            <button className="border border-slate-300 rounded-3xl bg-slate-50  shadow-2xl inline-block box-shadow" style={{ width: '200px', height: '280px' }}>
+            <button className="border border-slate-300 bg-slate-50  shadow-2xl inline-block box-shadow" style={{ width: '252px', height: '230px', borderRadius: '40px'}}>
               <div className="flex flex-col items-center">
                 <Image 
                 src={pythonImage}
                 alt="python"
                 className="course-img"
                 />
-                <h2>
+                <h2 className="font-semibold">
                   Python 101
                 </h2>
                 <p className="p-2 text-xs">
-                Python is a general-purpose, versatile, and powerful programming language. It's a great first language because Python code is concise and easy to read.
+                Python is a general-purpose, versatile, and powerful programming language. 
                 </p>
               </div>
             </button>
